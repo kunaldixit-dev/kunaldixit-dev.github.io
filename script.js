79baa81ddc8dc4a7da96e44a14c7f0e82c9bfef3
kunaldixit-dev <148314519+kunaldixit-dev@users.noreply.github.com>
2023-10-18 12:04:10 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function openpanel() {
+    document.getElementById("mySidepanel").style.width="250px";
+}
+
+function closepanel() {
+    document.getElementById("mySidepanel").style.width="0";
+}
+
